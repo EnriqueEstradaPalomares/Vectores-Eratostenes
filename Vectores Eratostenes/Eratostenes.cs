59bb94dd07f8b09fcc60e26abb9d6d199c215ee2
EnriqueEstradaPalomares <36722043+EnriqueEstradaPalomares@users.noreply.github.com>
2018-03-23 17:41:59 -0600

    hola

---
+++ Vectores Eratostenes/Eratostenes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vectores_Eratostenes
+{
+    class Eratostenes
+    {
+        private bool[] _vec;//vector booleano
+        public Eratostenes(int num)
+        {
+            _vec = new bool[num];
+        }
+        public string returnEratostenes()
+        {
+            string cad = "";//sirve para guardar la cadena
+            for(int i=0; i < _vec.Length; i++)
+            {
+                _vec[i] = true;//llenamos el vector de bool con verdaderos
+            }
+            for(int i = 2; i < _vec.Length; i++)//para i
+            {//con el for de j quitamos el verdadero de los multiplos de la posicion de i
+                for(int j = 2; i * j < _vec.Length; j++)
+                {
+                    _vec[i * j] = false;
+                }
+            }
+            for(int i=2; i < _vec.Length; i++)
+            {
+                if (_vec[i])//si en el bool[i] es un verdadero
+                    cad += i.ToString() +", ";//pone el valor de i en el string
+            }
+            return cad;//regresa el string
+        }
+    }
+}
---
+++ Vectores Eratostenes/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Vectores_Eratostenes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(317, 251);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 266);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "José Enrique Estrada Palomares 4D";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 288);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Primos \"Metodo Eratostenes\"";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
